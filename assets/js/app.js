084b3817abd227886ed6eebe8897f42fd02dd02c
FernandoLorca <florcapinto@gmail.com>
2022-11-29 10:50:39 -0300

    rendering id and list ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,68 @@
-"use strict";
+'use strict';
+
+const inputText = document.querySelector('input[type=text');
+const btnAdd = document.querySelector('#btnAdd');
+const resultTotal = document.querySelector('#resultTotal');
+const resultDone = document.querySelector('#resultDone');
+const idTask = document.querySelector('#idTask');
+const task = document.querySelector('#task');
+const inputCheckbox = document.querySelector('input[type=checkbox]');
+const deleteTask = document.querySelector('#deleteTask');
+const renderId = document.querySelector('#renderId');
+const renderTask = document.querySelector('#renderTask');
+
+const tasks = [];
+
+const renderTaskList = () => {
+  const lastObj = tasks[tasks.length - 1];
+
+  const render = `
+    <li>
+        <p id="task">${lastObj.task}</p>
+        <div class="todoContainer__todoList--tasks-doneDelete">
+        <input type="checkbox" />
+        <img
+            onclick="delete("
+            id="deleteTask"
+            src="./assets/img/x.svg"
+            alt="delete task"
+        />
+        </div>
+    </li>
+  `;
+
+  renderTask.innerHTML += render;
+};
+
+const renderIdNumber = () => {
+  const lastObj = tasks[tasks.length - 1];
+
+  const render = `
+  <li>
+      <p id="idTask">${lastObj.id}</p>
+  </li>
+`;
+
+  renderId.innerHTML += render;
+};
+
+btnAdd.addEventListener('click', () => {
+  tasks.push({
+    id: Date.now(),
+    task: inputText.value,
+  });
+
+  renderTaskList();
+
+  renderIdNumber();
+});
+
+// deleteTask.addEventListener('click', () => {
+//   tasks.map(mov => {
+//     console.log(mov);
+//   });
+// });
+
+// const deleteTaskSelected = id => {
+//   tawsks;
+// };
