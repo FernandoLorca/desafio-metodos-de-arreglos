2a7ec58f6350ea606b0ffb2f9def5ab9ee2cc237
FernandoLorca <florcapinto@gmail.com>
2022-12-02 11:39:42 -0800

    challenge done

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,58 +11,109 @@ const deleteTask = document.querySelector('#deleteTask');
 const renderId = document.querySelector('#renderId');
 const renderTask = document.querySelector('#renderTask');
 
-const tasks = [];
+let tasks = [
+  { id: 1669730370878, task: 'Trabajar', state: false },
+  { id: 1669730370873, task: 'Hacer ejercicio', state: false },
+  { id: 1669730369115, task: 'Estudiar js', state: false },
+];
 
-const renderTaskList = () => {
-  const lastObj = tasks[tasks.length - 1];
+const renderTaskSelected = () => {
+  renderTask.innerHTML = '';
 
-  const render = `
-    <li>
-        <p id="task">${lastObj.task}</p>
+  tasks.forEach(item => {
+    renderTask.innerHTML += `
+      <li>
+        <p class="${item.state ? 'clase' : ''}">${
+      item.task
+    } - <span class="selectedTask">${item.state ? 'Realizado' : ''} </span></p>
         <div class="todoContainer__todoList--tasks-doneDelete">
-        <input type="checkbox" />
-        <img
-            onclick="delete("
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          class="bi bi-check"
+          viewBox="0 0 16 16"
+          color="green"
+          onclick="doneTaskSelected(${item.id}, ${item.state})"
+        >
+          <path
+            d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"
+          />
+        </svg>
+          <img
             id="deleteTask"
             src="./assets/img/x.svg"
             alt="delete task"
-        />
+            onclick="deleteTaskSelected(${item.id})"
+          />
         </div>
-    </li>
-  `;
-
-  renderTask.innerHTML += render;
-};
-
-const renderIdNumber = () => {
-  const lastObj = tasks[tasks.length - 1];
-
-  const render = `
-  <li>
-      <p id="idTask">${lastObj.id}</p>
-  </li>
+      </li>
 `;
-
-  renderId.innerHTML += render;
+  });
 };
+
+const renderIdTadk = () => {
+  renderId.innerHTML = '';
+
+  tasks.forEach(item => {
+    renderId.innerHTML += `
+      <li>
+        <p id="idTask">${item.id}</p>
+      </li>
+`;
+  });
+};
+
+const doneTaskSelected = (id, state) => {
+  for (let index = 0; index < tasks.length; index++) {
+    if (tasks[index].id == id) {
+      tasks[index].state = true;
+      renderTaskSelected();
+      renderIdTadk();
+      updateTasksTotalDone();
+
+      return;
+    }
+  }
+};
+
+const deleteTaskSelected = id => {
+  tasks = tasks.filter(item => item.id !== id);
+  resultTotal.textContent = tasks.length;
+  renderTaskSelected();
+  renderIdTadk();
+  updateTasksTotalDone();
+};
+
+const updateTasksTotalDone = () => {
+  const prueba = tasks.reduce((acc, cur) => {
+    if (cur.state === true) {
+      acc.push(cur.id);
+    }
+
+    return acc;
+  }, []);
+
+  console.log(prueba);
+
+  resultDone.textContent = prueba.length;
+};
+
+resultTotal.textContent = tasks.length;
+renderTaskSelected();
+renderIdTadk();
 
 btnAdd.addEventListener('click', () => {
   tasks.push({
     id: Date.now(),
     task: inputText.value,
+    state: false,
   });
 
-  renderTaskList();
+  inputText.value = '';
+  resultTotal.textContent = tasks.length;
 
-  renderIdNumber();
+  renderTaskSelected();
+  renderIdTadk();
 });
-
-// deleteTask.addEventListener('click', () => {
-//   tasks.map(mov => {
-//     console.log(mov);
-//   });
-// });
-
-// const deleteTaskSelected = id => {
-//   tawsks;
-// };
